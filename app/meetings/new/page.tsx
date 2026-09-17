8679a382bdc2906ffd52c5a69569adef22487775
Segundo Alvarez <segundoav@hotmail.com>
2026-09-17 00:52:18 -0500

    Clean up imports in new meeting page

--- app/meetings/new/page.tsx
+++ app/meetings/new/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from 'react'; // Nota: usaremos useState de React
+import { useState } from 'react'; 
 import { useRouter } from 'next/navigation';
 import { addMeeting } from '@/lib/meetings-db';
 import Link from 'next/link';
